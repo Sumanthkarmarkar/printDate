4de4af0463cf87f1956bb9a3685a5b29c4af5d33
Sumanthkarmarkar <sumanthkar2000@gmail.com>
2023-09-19 16:19:45 +0530

    Initial commit

---
+++ src/printDate/Main.java
@@ -0,0 +1,9 @@
+package printDate;
+import java.util.Date;
+public class Main {
+    public static void main(String[] args)
+    {
+    	System.out.println("this is a java project");
+    	System.out.println(new Date());
+    }
+}
